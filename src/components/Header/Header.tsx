e5deabaaa35bc9ae30806637233c2394be0e4de2
Fallougin <artursuvor@mail.ru>
2024-03-19 15:06:19 +0100

    v1.0

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,9 +5,11 @@ const Header: React.FC = () => {
 
     return (
         <header>
-            <p>Home</p>
-            <p>About me</p>
-            <p>Portfolio</p>
+            <div className="header-container">
+                <p>Home</p>
+                <p>About me</p>
+                <p>Portfolio</p>
+            </div>  
         </header>
     );  
 };
